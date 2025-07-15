e8c6d654c4801259f2f5f34e450899eff5a60d3b
sahils576 <smashingsharma274@gmail.com>
2025-07-15 16:20:40 +0000

    datatype

---
+++ basics/01varabl.js
@@ -0,0 +1,12 @@
+const accountid=123344
+let emailid="smashingsharma274@gmail.com"
+var accountpassword="12345"
+accountcITY="JAIPUR"
+
+console.log( accountid);
+
+emailid= "yoyo.gmail.com"
+accountpassword="6868"
+accountcITY="benagluru"
+console.table([ accountid, emailid ,accountpassword ,accountcITY])
+/* prefer not to use var*/
