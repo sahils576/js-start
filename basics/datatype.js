c1ce43c98bf89dff25db80f06fee36f9316129ab
sahils576 <smashingsharma274@gmail.com>
2025-07-16 17:16:35 +0000

    data con

---
+++ basics/datatype.js
@@ -0,0 +1,14 @@
+let score=56 // undefined agr leete hai to nan and for null or boolean it gives nan
+let sio="sjtabc45"
+console.log(typeof (score));
+console.log(typeof (sio));
+let valuenum=Number(score);
+let valuenum1=Number(sio);
+console.log(typeof (valuenum));
+console.log(typeof (valuenum1));
+console.log( (valuenum1));
+let loogedin=1;
+let bool=Boolean(loogedin)
+console.log(typeof loogedin)
+console.log(loogedin)
+console.log(bool)
