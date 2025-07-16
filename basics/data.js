05f1c86a3b6051ef799fe381d24e2fff4e598a84
sahils576 <smashingsharma274@gmail.com>
2025-07-16 17:52:46 +0000

    nn

---
+++ basics/data.js
@@ -0,0 +1,16 @@
+console.log("2">1)
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+console.log("2"===2)
+const num=Symbol("123")
+const num1=Symbol("123")
+console.log(num ==num1)
+const heroes=["shaktiman" ,"doga","dogesh"];
+let myobj={
+    name:"hitesh",
+    age:67
+}
+const myfunction= function(){
+    console.log("hello world");
+}
